bd7848fca2158225123ff2bc2cfcd7f5323e16e4
Antoine Félix <felixantoine67@yahoo.fr>
2020-01-07 12:22:51 +0100

    UPDATED GridGUI to handle multiplayer.

--- src/view/GridGUI.java
+++ src/view/GridGUI.java
@@ -8,7 +8,17 @@ import java.awt.GridLayout;
 import java.awt.event.*;
 import java.util.Random;
 
-import control.*;
+import control.Game;
+import control.PlayerManager;
+
+import java.awt.BorderLayout;
+
+import java.util.Dictionary;
+
+import java.util.Hashtable;
+
+import javax.swing.JLabel;
+
 import model.State;
 
 /**
@@ -20,75 +30,89 @@ public class GridGUI extends JPanel {
     // Attributes
     private static final long serialVersionUID = 6542103327377268914L;
     private JButton[][] units;
-    private int cellsLeft;
-    private int gridWidth;
-    private int gridHeight;
+    private int cells_left;
+    private int grid_width;
+    private int grid_height;
     private Game game;
-    private Color playerColor;
-    private PlayerManager playerManager;
-    private int player;
+    private Dictionary<Integer, Color> player_colors;
+    private PlayerManager player_manager;
+    private int[] players;
+    private int player_cursor;
+    private JLabel jlTitle;
+    private JPanel jpGrid;
 
     //Methods
 
-    public GridGUI(int gridWidth, int gridHeight, int player) {
+    public GridGUI(int grid_height, int grid_width) {
         super();
 
-        this.gridWidth = gridWidth;
-        this.gridHeight = gridHeight;
+        this.grid_height = grid_height;
+        this.grid_height = grid_width;
         this.game = Game.getInstance();
-        this.cellsLeft = game.getStartCellsNumber();
-        this.playerColor = genPlayerColor();
-        this.playerManager = PlayerManager.getInstance();
-        this.player = player;
-        State[][] statesGrid = playerManager.getPlayerGrid(player);
+        this.cells_left = game.getStartCellsNumber();
+        this.player_manager = PlayerManager.getInstance();
+        this.players = player_manager.getPlayerIds();
+        this.player_colors = new Hashtable();
+        this.jlTitle = new JLabel("salut antoine");
+        this.jpGrid = new JPanel(new GridLayout(grid_height, grid_width));
+        
+        for (int i = 0; i < this.players.length; i++) {
+            this.player_colors.put(this.players[i], genPlayerColor());
+        }
+        
+        this.player_cursor = 0;
 
-        units = new JButton[gridWidth][gridHeight];
-        this.setLayout(new GridLayout(gridWidth, gridHeight));      
+        units = new JButton[grid_height][grid_width];
+        this.setLayout(new BorderLayout());      
+        this.add(jlTitle);
+        this.add(jpGrid);
 
         // Grid cells initialization
-        for (int i = 0; i < gridWidth; i++) {
-            for (int j = 0; j < gridHeight; j++) {
+        for (int i = 0; i < grid_height; i++) {
+            for (int j = 0; j < grid_width; j++) {
                 units[i][j] = new JButton();
-                if (statesGrid[i][j].equals(State.ALIVE)) {
-                    units[i][j].setBackground(playerColor);
-                }
-                else if (statesGrid[i][j].equals(State.DEAD)) {
-                    units[i][j].setBackground(Color.WHITE);
-                }
+                units[i][j].setBackground(Color.WHITE);
                 units[i][j].addActionListener( e -> unitClickHandler(e) );
-                this.add(units[i][j]);
+                jpGrid.add(units[i][j]);
             }
         }
     }
 
     public void unitClickHandler(ActionEvent e) {
         JButton btn = (JButton)e.getSource();
-        
-        if (btn.getBackground().equals(Color.WHITE)) {
-            if (this.cellsLeft > 0) {
-                btn.setBackground(this.playerColor);
-                this.cellsLeft--;
-            }  
-        }
-        if (cellsLeft == 0) {
-            this.playerManager.setPlayerGrid(this.player, getInitCells());
-            startGame();
-        }
-    }
 
-    public State[][] getInitCells() {
-        State[][] res = new State[this.gridWidth][this.gridHeight];
-        for (int i = 0; i < this.gridWidth; i++) {
-            for (int j = 0; j < this.gridHeight; j++) {
-                if ( this.units[i][j].getBackground().equals(this.playerColor) ) {
-                    res[i][j] = State.ALIVE;
-                }
-                else {
-                    res[i][j] = State.DEAD;
+        if (btn.getBackground().equals(Color.WHITE)) {
+                if (this.cells_left > 0) {
+                    btn.setBackground(this.player_colors.get(this.players[this.player_cursor]));
+                    this.player_cursor++;
+                    if (this.player_cursor >= this.players.length) {
+                        this.player_cursor -= this.players.length;
+                        this.cells_left--;
+                    }
+                else if (cells_left == 0) {
+                    initCells();
+                    startGame();
                 }
             }
         }
-        return res;
+    }
+
+    public void initCells() {
+        State[][] cells;
+        for (int player : this.players) {
+            cells = new State[this.grid_height][this.grid_width];
+            for (int i = 0; i < this.grid_height; i++) {
+                for (int j = 0; j < this.grid_width; j++) {
+                    if (this.units[i][j].getBackground().equals(this.player_colors.get(player))) {
+                        cells[i][j] = State.ALIVE;
+                    }
+                    else {
+                        cells[i][j] = State.DEAD;
+                    }
+                }
+            }
+            player_manager.setPlayerGrid(player, cells.clone());
+        }
     }   
 
     public Color genPlayerColor() {
@@ -99,23 +123,15 @@ public class GridGUI extends JPanel {
         return new Color(r, g, b);
     }
 
-    public void startGame() {
-        // Disable all units action listeners
-        for (int i = 0; i < this.gridWidth; i++) {
-            for (int j = 0; j < this.gridHeight; j++) {
+    private void startGame() {
+        // Disable all units action listeners.
+        for (int i = 0; i < this.grid_height; i++) {
+            for (int j = 0; j < this.grid_width; j++) {
                 for (ActionListener a : units[i][j].getActionListeners()) {
                     units[i][j].removeActionListener(a);
                 }
             }
         }
-        // starting game in itself
-    }
-
-    public void setCellColor(int x, int y, Color color) {
-        this.units[x][y].setBackground(color);
-    }
-
-    public Color getPlayerColor() {
-        return playerColor;
+        // Starting the simulation.
     }
 }
